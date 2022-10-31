9fb0a1792e1f35dd08b99c3eefee9b3c8b8daf55
Willian Lopes <williangldzn@gmail.com>
2022-10-31 20:45:49 -0300

    use ansible requiment to install community roles

--- run-ansible-archlinux.sh
+++ run-ansible-archlinux.sh
@@ -2,20 +2,23 @@
 echo "=== Install Ansible ==="
 sudo pacman -S --noconfirm ansible
 
-echo "=== Install ansible galaxy community general ==="
-ansible-galaxy collection install community.general
+# echo "=== Install ansible galaxy community general ==="
+# ansible-galaxy collection install community.general
 
-echo "=== Install ansible galaxy AUR role ==="
-ansible-galaxy install kewlfft.aur
+# echo "=== Install ansible galaxy AUR role ==="
+# ansible-galaxy collection install kewlfft.aur
+
+echo "=== Install ansible galaxy community packages ==="
+ansible-galaxy collection install -r requirements.yml
 
 echo "=== Running ansible base ==="
-ansible-playbook install-archlinux-base-dependencies.yml --ask-become-pass
+ansible-playbook install-archlinux-base-dependencies.yml --ask-become-pass -v
 
 echo "=== Running ansible music ==="
-ansible-playbook install-archlinux-music-dependencies.yml --ask-become-pass
+ansible-playbook install-archlinux-music-dependencies.yml --ask-become-pass -v
 
 echo "=== Running ansible work ==="
-ansible-playbook install-archlinux-work-dependencies.yml --ask-become-pass
+ansible-playbook install-archlinux-work-dependencies.yml --ask-become-pass -v
 
 echo "change the default shell for zsh"
 if [ $SHELL != '/usr/bin/zsh' ]
