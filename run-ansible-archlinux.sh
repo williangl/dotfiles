3eff51a79d9c1d7ece1cc5cba6e980891ad41971
Willian Lopes <williangldzn@gmail.com>
2023-04-02 16:49:16 -0300

    improvements on ansible run script

--- run-ansible-archlinux.sh
+++ run-ansible-archlinux.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "\n"
+echo "\n"
 echo "=== Install Ansible ==="
 sudo pacman -S --noconfirm ansible
 
@@ -7,19 +9,33 @@ sudo pacman -S --noconfirm ansible
 
 # echo "=== Install ansible galaxy AUR role ==="
 # ansible-galaxy collection install kewlfft.aur
-
+echo "\n"
+echo "\n"
 echo "=== Install ansible galaxy community packages ==="
-ansible-galaxy collection install -r requirements.yml
+ansible-galaxy collection install -r ansible_playbook/requirements.yml
 
+echo "\n"
+echo "\n"
 echo "=== Running ansible base ==="
-ansible-playbook install-archlinux-base-dependencies.yml --ask-become-pass -v
+ansible-playbook ansible_playbook/install-archlinux-base-dependencies.ansible.yml --ask-become-pass -v
 
+echo "\n"
+echo "\n"
 echo "=== Running ansible music ==="
-ansible-playbook install-archlinux-music-dependencies.yml --ask-become-pass -v
+ansible-playbook ansible_playbook/install-archlinux-music-dependencies.ansible.yml --ask-become-pass -v
 
+echo "\n"
+echo "\n"
 echo "=== Running ansible work ==="
-ansible-playbook install-archlinux-work-dependencies.yml --ask-become-pass -v
+ansible-playbook ansible_playbook/install-archlinux-work-dependencies.ansible.yml --ask-become-pass -v
 
+echo "\n"
+echo "\n"
+echo "=== Running ansible game and video ==="
+ansible-playbook ansible_playbook/install-archlinux-game-video-dependencies.ansible.yml --ask-become-pass -v
+
+echo "\n"
+echo "\n"
 echo "change the default shell for zsh"
 if [ $SHELL != '/usr/bin/zsh' ]
 then
